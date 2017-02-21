db1543e40f9badc79ac2ae913340e8229985d713
Dmitry Nazarov <mail@nazarov.tech>
2017-02-22 00:42:34 +0300

    Format code

--- run.py
+++ run.py
@@ -7,22 +7,22 @@ DATA = np.genfromtxt("data.txt", dtype=int)
 SIZE = 4
 
 
-def count_occurrences (data, pattern):
-    '''Find all occurrences of pattern and count them'''
-    pattern = np.fromstring (pattern, dtype=int, sep=' ')
+def count_occurrences(data, pattern):
+    """Find all occurrences of pattern and count them"""
+    pattern = np.fromstring(pattern, dtype=int, sep=' ')
     pattern_len = np.shape(pattern)[0]
-    data_len = np.shape(data)[0] # data[data_len] is not already allowed to call
+    data_len = np.shape(data)[0]  # data[data_len] is not already allowed to call
     occurrences_amount = 0
     index = 0
     for value in data:
-        if value == pattern[0]: # possible start of pattern
+        if value == pattern[0]:  # possible start of pattern
             start = index
-            if start + pattern_len >= data_len: # if pattern doesn't fit into sequence
+            if start + pattern_len >= data_len:  # if pattern doesn't fit into sequence
                 break
-            else: # check is it pattern
+            else:  # check is it pattern
                 is_match = True
                 for p in xrange(pattern_len):
-                    if data[start + p] != pattern[p]: # it is not
+                    if data[start + p] != pattern[p]:  # it is not
                         is_match = False
                         break
                 if is_match:
@@ -32,7 +32,7 @@ def count_occurrences (data, pattern):
 
 
 def gen_2d_frequency_matrix(data, size):
-    '''Generates 2d frequency matrix'''    
+    '''Generates 2d frequency matrix'''
     frequency_matrix = np.empty((size, size))
     for i in xrange(size):
         for j in xrange(size):
@@ -42,7 +42,7 @@ def gen_2d_frequency_matrix(data, size):
 
 
 def gen_3d_frequency_matrix(data, size):
-    '''Generates 3d frequency matrix'''    
+    """Generates 3d frequency matrix"""
     frequency_matrix = np.zeros((size ** 2, size))
     import itertools
     patterns = []
@@ -57,8 +57,8 @@ def gen_3d_frequency_matrix(data, size):
 
 
 if __name__ == '__main__':
-#    array_string = ' '.join(str(x) for x in DATA)
-#    print array_string
+    #    array_string = ' '.join(str(x) for x in DATA)
+    #    print array_string
     matrix = gen_2d_frequency_matrix(DATA, SIZE)
     print matrix, '\n'
     matrix = gen_3d_frequency_matrix(DATA, SIZE)
